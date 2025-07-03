0bb7447993387f76488f196f5d4af6eaf23ef3d3
gbenmoese <ese.gbenmo@uricreative.com>
2025-07-03 20:47:26 +0100

    hero fixes

--- src/Components/LandingPage/Hero.jsx
+++ src/Components/LandingPage/Hero.jsx
@@ -9,17 +9,17 @@ const Hero = () => {
 	return (
 		<>
 			<section className='bg-[#ff5a3d] relative overflow-hidden pt-26 -mb-32'>
-				<div className='max-w-7xl py-8 flex items-center min-h-screen'>
-					<div className='flex w-full items-center justify-between absolute md:pl-60'>
+				<div className='max-w-7xl py-8 flex items-center min-h-[60vh] md:min-h-screen'>
+					<div className='flex w-full items-center justify-between absolute lg:pl-60'>
 						{/* Left Content */}
-						<div className='relative text-white text-left max-w-7xl z-30 ml-5 lg:ml-5 md:ml-0 sm:ml-0 md:max-w-full sm:max-w-full md:px-4 sm:px-4'>
-							<div className='font-semibold text-2xl lg:text-6xl md:text-4xl sm:text-3xl leading-tight mb-8'>
+						<div className='relative text-white text-left w-full mx-auto z-30 ml-5  sm:ml-0 sm:px-4 sm:max-w-3xl md:max-w-full md:ml-0 md:px-4  md:mx-0 lg:ml-5 '>
+							<div className='font-semibold text-2xl  md:text-4xl  md:text-left lg:text-6xl leading-tight mb-8 text-center'>
 								<h1>You deserve a </h1>
-								<span className='font-bold text-[82px] lg:text-[82px] md:text-5xl sm:text-4xl caveat'>
+								<span className='font-bold text-5xl lg:text-[82px] md:text-5xl sm:text-4xl caveat'>
 									welcoming, loving world.
 								</span>
 							</div>
-							<p className='text-xl lg:text-xl md:text-lg sm:text-base leading-8 max-w-md lg:max-w-md md:max-w-full sm:max-w-full'>
+							<p className='text-sm text-center md:text-left lg:text-xl md:text-lg sm:text-base leading-8 max-w-xs mx-auto md:mx-0 lg:max-w-md md:max-w-full sm:max-w-full'>
 								And so do the people you care about. Here you can reach out to a
 								counselor if you're struggling, find answers and information,
 								and get the tools you need to help someone else.
@@ -27,7 +27,7 @@ const Hero = () => {
 						</div>
 
 						{/* Right Images - DESKTOP ONLY - Keep exactly as original */}
-						<div className='absolute flex-shrink-0 z-10 right-0 mt-8 pr-8 hidden lg:block'>
+						<div className='absolute flex-shrink-0 z-10 right-0 mt-8 pr-8 hidden md:block'>
 							{/* First Image - Top, Tilted Left */}
 							<div className='relative z-10 mb-4 right-40 top-8'>
 								<img
@@ -88,10 +88,10 @@ const Hero = () => {
 				</div>
 
 				{/* MOBILE/TABLET ONLY - All images at bottom */}
-				<div className='block lg:hidden pt-8 pb-16'>
-					<div className='relative px-4 max-w-4xl mx-auto min-h-[500px]'>
+				<div className='block md:hidden pt-8 pb-16'>
+					<div className='relative px-4 max-w-4xl mx-auto min-h-[400px]'>
 						{/* Image 1 - Bottom left */}
-						<div className='absolute left-4 bottom-0 w-32 h-40 md:w-40 md:h-48 z-10'>
+						<div className='absolute left-4 bottom-0 w-32 h-40  z-10'>
 							<img
 								src={image1}
 								alt='Smiling person in yellow cardigan'
@@ -100,7 +100,7 @@ const Hero = () => {
 						</div>
 
 						{/* Image 2 - Center left, overlapping */}
-						<div className='absolute left-12 bottom-24 w-28 h-36 md:w-36 md:h-44 z-20'>
+						<div className='absolute left-12 bottom-24 w-28 h-36  z-20'>
 							<img
 								src={image2}
 								alt='Smiling person'
@@ -109,7 +109,7 @@ const Hero = () => {
 						</div>
 
 						{/* Image 3 - Top center */}
-						<div className='absolute left-1/2 top-0 transform -translate-x-1/2 w-36 h-44 md:w-44 md:h-52 z-30'>
+						<div className='absolute left-1/2 top-0 transform -translate-x-1/2 w-36 h-44  z-30'>
 							<img
 								src={image3}
 								alt='Black person in pink top'
@@ -118,7 +118,7 @@ const Hero = () => {
 						</div>
 
 						{/* Image 4 - Center right, largest */}
-						<div className='absolute right-4 top-16 w-40 h-48 md:w-48 md:h-56 z-40'>
+						<div className='absolute right-4 top-16 w-40 h-48 z-40'>
 							<img
 								src={image4}
 								alt='Black person in pink top'
@@ -127,7 +127,7 @@ const Hero = () => {
 						</div>
 
 						{/* Image 5 - Bottom center */}
-						<div className='absolute left-1/2 bottom-8 transform -translate-x-1/2 w-32 h-40 md:w-40 md:h-48 z-15'>
+						<div className='absolute left-1/2 bottom-8 transform -translate-x-1/2 w-32 h-40  z-15'>
 							<img
 								src={image5}
 								alt='Black person in pink top'
@@ -136,7 +136,7 @@ const Hero = () => {
 						</div>
 
 						{/* Image 6 - Bottom right */}
-						<div className='absolute right-8 bottom-32 w-28 h-36 md:w-36 md:h-44 z-25'>
+						<div className='absolute right-8 bottom-32 w-28 h-36  z-25'>
 							<img
 								src={image6}
 								alt='Black person in pink top'
